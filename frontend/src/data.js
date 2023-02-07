17cd4570de2294f68587f5209b0fdb9c8d001edf
Kaboudouo <chotto60@hotmail.com>
2023-02-07 10:35:29 -0500

    Created Product Data, Printed Products on front page, Stylized Products

---
+++ frontend/src/data.js
@@ -0,0 +1,84 @@
+const data = {
+  products: [
+    {
+      name: 'PlayStation 5 Console',
+      slug: 'playstation5-console',
+      category: 'Consoles',
+      image: '/images/p5.jpg',
+      price: 499,
+      countInStock: 40,
+      brand: 'Sony',
+      rating: 4.8,
+      numReviews: 45,
+      description:
+        "PlayStation's latest and greatest console. Take a deep dive into new cinematic masterpieces fully equiped.",
+    },
+    {
+      name: 'Xbox Series X Console',
+      slug: 'xbox-seriesx-console',
+      category: 'Consoles',
+      image: '/images/xx.jpg',
+      price: 449,
+      countInStock: 110,
+      brand: 'Microsoft',
+      rating: 4.6,
+      numReviews: 26,
+      description:
+        "Xbox's latest generation of console. Unrivaled power gives you the power to fully immerse yourself in gameplay.",
+    },
+    {
+      name: 'Nintendo Switch Console',
+      slug: 'nintendo-switch-console',
+      category: 'Consoles',
+      image: '/images/ns.jpg',
+      price: 399,
+      countInStock: 320,
+      brand: 'Nintendo',
+      rating: 4.9,
+      numReviews: 113,
+      description:
+        "Fun at home and on the go! Play Nintendo's greatest hits either as a home console or in the Nintendo Switch's signature hand-held mode.",
+    },
+    {
+      name: 'PlayStation 5 DualSense Controller',
+      slug: 'playstation-dualshock-controller',
+      category: 'Peripherals',
+      image: '/images/p5_cont.png',
+      price: 59,
+      countInStock: 144,
+      brand: 'Sony',
+      rating: 4.4,
+      numReviews: 29,
+      description:
+        'The revolutionary DualShock Playstation 5 controllers feature haptic feedback and optimaly engineered ergonamic grips.',
+    },
+    {
+      name: 'Xbox Controller',
+      slug: 'xbox-controller',
+      category: 'Peripherals',
+      image: '/images/xx_cont.jpg',
+      price: 59,
+      countInStock: 80,
+      brand: 'Microsoft',
+      rating: 4.1,
+      numReviews: 19,
+      description:
+        "Elevate your play with Xbox's greatest controller design. Auxilary components not included.",
+    },
+    {
+      name: 'Nintendo Switch Pro Controller',
+      slug: 'nintendo-pro-controller',
+      category: 'Peripherals',
+      image: '/images/npc.PNG',
+      price: 89,
+      countInStock: 214,
+      brand: 'Nintendo',
+      rating: 3.9,
+      numReviews: 204,
+      description:
+        "Use Nintendo's new pro controller. Features motion sensors and haptic feedback.",
+    },
+  ],
+};
+
+export default data;
