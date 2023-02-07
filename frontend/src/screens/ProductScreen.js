665e624511e499b546ce3de8c78ac647525994dd
Kaboudouo <chotto60@hotmail.com>
2023-02-07 13:53:30 -0500

    Created Routing for Product page

---
+++ frontend/src/screens/ProductScreen.js
@@ -0,0 +1,19 @@
+import { useParams } from 'react-router-dom';
+import data from '../data';
+
+function ProductScreen() {
+  const params = useParams();
+  const { slug } = params;
+  return (
+    <div>
+      <h1>{slug}</h1>
+      <div className="product-page">
+        {data.products.map((product) => (
+          <div className=""></div>
+        ))}
+      </div>
+    </div>
+  );
+}
+
+export default ProductScreen;
