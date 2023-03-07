091170fa64b49c689a2fdaa262dc6b28cc46c645
Kaboudouo <chotto60@hotmail.com>
2023-03-07 17:32:09 -0500

    Created user profile page

--- backend/routes/userRoute.js
+++ backend/routes/userRoute.js
@@ -2,7 +2,7 @@ import express from 'express';
 import User from '../models/userModel.js';
 import expressAsyncHandler from 'express-async-handler';
 import bcrypt from 'bcryptjs';
-import { generateToken } from '../utils.js';
+import { generateToken, isAuth } from '../utils.js';
 
 const userRouter = express.Router();
 
@@ -29,12 +29,12 @@ userRouter.post(
 userRouter.post(
   '/signup',
   expressAsyncHandler(async (req, res) => {
-    const newUSer = new User({
+    const newUser = new User({
       name: req.body.name,
       email: req.body.email,
       password: bcrypt.hashSync(req.body.password),
     });
-    const user = await newUSer.save();
+    const user = await newUser.save();
     res.send({
       _id: user.id,
       name: user.name,
@@ -42,6 +42,33 @@ userRouter.post(
       isAdmin: user.idAdmin,
       token: generateToken(user),
     });
+  })
+);
+
+userRouter.put(
+  '/profile',
+  isAuth,
+  expressAsyncHandler(async (req, res) => {
+    const user = await User.findById(req.user._id);
+    if (user) {
+      user.name = req.body.name || user.name;
+      user.email = req.body.email || user.email;
+      if (req.body.password) {
+        user.password = bcrypt.hashSync(req.body.password, 8);
+      }
+
+      const updatedUser = await user.save();
+
+      res.send({
+        _id: updatedUser._id,
+        name: updatedUser.name,
+        email: updatedUser.email,
+        isAdmin: updatedUser.isAdmin,
+        token: generateToken(updatedUser),
+      });
+    } else {
+      res.status(404).send({ message: 'User not found' });
+    }
   })
 );
 
